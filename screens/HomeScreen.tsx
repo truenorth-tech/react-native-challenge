6165b49606a3016edd977360bcbb35e44c143bbb
rfahernandez <rodrigohernandez6684@gmail.com>
2022-03-19 20:40:27 -0300

    - expo challenge replaced by cli one

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import {Alert, StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
-/* 
-  Implement form using any user/pass combination 
+/*
+  Implement form using any user/pass combination
   Store data using React context
   💯 Handling Sensitive Info and Secure Storage is a great plus
 */
@@ -10,10 +10,10 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome</Text>
-      <Text></Text>
-      <TextInput style={{ height: 40 }} placeholder="Enter your name" />
-      <TextInput style={{ height: 40 }} placeholder="Enter your passowrd" />
-      <Button title="Sign in" onPress={() => alert('List')} />
+      <Text />
+      <TextInput style={{height: 40}} placeholder="Enter your name" />
+      <TextInput style={{height: 40}} placeholder="Enter your passowrd" />
+      <Button title="Sign in" onPress={() => Alert.alert('List')} />
     </View>
   );
 }
--- screens/ToDoScreen.tsx
+++ screens/ToDoScreen.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import {Alert, StyleSheet, Text, View, Image, Button} from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Image style={styles.illustration} source={require('../assets/home-illustration.png')} />
+      <Image
+        style={styles.illustration}
+        source={require('../assets/home-illustration.png')}
+      />
       <Text style={styles.title}>Howdy partner!</Text>
       <Text>This is your assignment.</Text>
       <Text>Follow the instructions on the Readme file.</Text>
-      <Text>Don’t worry, it’s easy! You should have the app looking smooth in no time.</Text>
-      <Button title="Start Here" onPress={() => alert('Home')} />
+      <Text>
+        Don’t worry, it’s easy! You should have the app looking smooth in no
+        time.
+      </Text>
+      <Button title="Start Here" onPress={() => Alert.alert('Home')} />
     </View>
   );
 }
